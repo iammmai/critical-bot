7ba5c4eae409e4f4b489360b65a31495a7b353e1
johann <joh.hackler@gmail.com>
2021-05-25 23:51:41 +0200

    remove !

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ bot.start((ctx) =>
   bot.telegram
     .sendMessage(
       ctx.chat.id,
-      "Hi there! I am the critical bot. I will send you everyday *at 9 am one multiple choice question*. You can also type /question to receive an *open question* and /help for further information! I'm developed by students from different backgrounds as a university project. If you want to send us *feedback* just write /feedback [your text]",
+      "Hi there. I am the critical bot. I will send you everyday *at 9 am one multiple choice question*. You can also type /question to receive an *open question* and /help for further information. I'm developed by students from different backgrounds as a university project. If you want to send us *feedback* just write /feedback [your text]",
       { parse_mode: "MarkdownV2" }
     )
     .then((msg) => {
